3c104619976e9e6b9b5485007406e291cbaa0dd6
Savithri Marati <savithri62001@gmail.com>
2025-05-29 19:51:04 +0530

    Update views.py

--- python-web-app/polls/views.py
+++ python-web-app/polls/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),  # for /demo/
+]
+
 
 # Create your views here.
