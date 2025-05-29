2f32ad77cfa6fabdeb8c1bf1508d4f4d6d67562a
Savithri Marati <savithri62001@gmail.com>
2025-05-29 22:25:27 +0530

    Update views.py

--- python-web-app/polls/views.py
+++ python-web-app/polls/views.py
@@ -1,10 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # for /demo/
-]
 
 
-# Create your views here.
+def index(request):
+    return render(request, 'demo_site.html')
